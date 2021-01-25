8208386e5726d27f68a5d7377cb3b80c007e9eb8
yuanbao97 <316261393@qq.com>
2021-01-25 13:29:21 +0800

    5.5 优化取消焦点

--- app/src/main/java/com/example/forcedofflinedemo/activity/LoginActivity.java
+++ app/src/main/java/com/example/forcedofflinedemo/activity/LoginActivity.java
@@ -16,6 +16,9 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import com.example.forcedofflinedemo.R;
 import com.example.forcedofflinedemo.framework.BaseActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -29,8 +32,6 @@ public class LoginActivity extends BaseActivity {
     EditText passwordEditView;
     @BindView(R.id.button)
     Button button;
-    @BindView(R.id.loginLayout)
-    ConstraintLayout loginLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +40,14 @@ public class LoginActivity extends BaseActivity {
         ButterKnife.bind(this);
     }
 
+    @Override
+    public void clearFocus() {
+        accountEditView.clearFocus();
+        passwordEditView.clearFocus();
+    }
 
-    @OnClick({R.id.button, R.id.loginLayout})
+
+    @OnClick({R.id.button})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button:
@@ -55,58 +62,8 @@ public class LoginActivity extends BaseActivity {
                     Toast.makeText(this, "账号或密码错误", Toast.LENGTH_SHORT).show();
                 }
                 break;
-            case R.id.loginLayout:
-                accountEditView.clearFocus();
-                passwordEditView.clearFocus();
-                break;
         }
     }
 
-    //时间分发方法重写
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        //如果是点击事件，获取点击的view，并判断是否要收起键盘
-        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
-            //获取目前得到焦点的view
-            View v = getCurrentFocus();
-            //判断是否要收起并进行处理
-            if (isShouldHideKeyboard(v, ev)) {
-                hideKeyboard(v.getWindowToken());
-            }
-        }
-        //这个是activity的事件分发，一定要有，不然就不会有任何的点击事件了
-        return super.dispatchTouchEvent(ev);
-    }
 
-    //判断是否要收起键盘
-    private boolean isShouldHideKeyboard(View v, MotionEvent event) {
-        //如果目前得到焦点的这个view是editText的话进行判断点击的位置
-        if (v instanceof EditText) {
-            int[] l = {0, 0};
-            v.getLocationInWindow(l);
-            int left = l[0],
-                    top = l[1],
-                    bottom = top + v.getHeight(),
-                    right = left + v.getWidth();
-            // 点击EditText的事件，忽略它。
-            return !(event.getX() > left) || !(event.getX() < right)
-                    || !(event.getY() > top) || !(event.getY() < bottom);
-        }
-        // 如果焦点不是EditText则忽略，这个发生在视图刚绘制完，第一个焦点不在EditText上
-        return false;
-    }
-
-    //隐藏软键盘并让editText失去焦点
-    private void hideKeyboard(IBinder token) {
-        accountEditView.clearFocus();
-        passwordEditView.clearFocus();
-        if (token != null) {
-            //这里先获取InputMethodManager再调用他的方法来关闭软键盘
-            //InputMethodManager就是一个管理窗口输入的manager
-            InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            if (im != null) {
-                im.hideSoftInputFromWindow(token, InputMethodManager.HIDE_NOT_ALWAYS);
-            }
-        }
-    }
 }
